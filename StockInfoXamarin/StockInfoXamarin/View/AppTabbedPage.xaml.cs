80de88dbe50f99164c13b88bbc9e63197917d714
maciuquk <maciejromik@gmai.com>
2021-01-10 10:33:57 +0100

    Add project files.

---
+++ StockInfoXamarin/StockInfoXamarin/View/AppTabbedPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StockInfoXamarin.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppTabbedPage : TabbedPage
+    {
+        public AppTabbedPage()
+        {
+            InitializeComponent();
+
+            //this.Children.Add(new ContentPage());
+            //this.Children.Add(new MainPage());
+            //this.Children.Add(new ContentPage());
+        }
+    }
+}
